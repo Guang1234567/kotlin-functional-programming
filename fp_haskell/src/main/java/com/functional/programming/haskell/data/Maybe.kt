79b3c4eefb1ebae249281241f92d0a4043b8b7ec
Guang1234567 <lihansey163@163.com>
2019-08-28 19:02:01 +0800

    tmp

--- fp_haskell/src/main/java/com/functional/programming/haskell/data/Maybe.kt
+++ fp_haskell/src/main/java/com/functional/programming/haskell/data/Maybe.kt
@@ -2,10 +2,10 @@ package com.functional.programming.haskell.data
 
 import com.functional.programming.haskell.typeclass.Monad
 
-sealed class Maybe<out T> : Monad<Maybe<T>> {
+sealed class Maybe<out T> : Monad<T> {
     object Nothing : Maybe<kotlin.Nothing>() {
 
-        override fun <R> binding(f: (kotlin.Nothing) -> Maybe<R>): Maybe<R> = this
+        override fun <R> binding(f: (kotlin.Nothing) -> Monad<R>): Maybe<R> = this
 
         override fun toString(): String = "Maybe.Nothing"
     }
@@ -13,6 +13,6 @@ sealed class Maybe<out T> : Monad<Maybe<T>> {
     data class Just<out T>(val value: T) : Maybe<T>() {
         override fun toString(): String = "Maybe.Just { $value }"
 
-        override fun <R> binding(f: (T) -> Maybe<R>): Maybe<R> = f(this.value)
+        override fun <R> binding(f: (T) -> Monad<R>): Monad<R> = f(this.value)
     }
 }
